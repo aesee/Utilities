8094844652d3c2f96aef95554a6600a051e19e6c
Alexey Sarapulov <aesee@mail.ru>
2017-10-10 22:49:20 +0600

    Changes in tests

--- Memory Perfomance Test/general_lib.asm
+++ Memory Perfomance Test/general_lib.asm
@@ -1,27 +1,31 @@
 # extern "C"
 # {
 #	 void setup_stack_pointer(long unsigned int);
-#	 unsigned long int timer(void);
+#	 unsigned long int cycle(void);
 #	 void write_result(int number);
+#	 void cycle_clear(void);
 # }
 
 
 .global setup_stack_pointer
-.global timer_clear
-.global timer
+.global cycle_clear
+.global cycle
 .global write_result
+
+FF64:
+.dword 0xffffffffffffffff
 
 setup_stack_pointer:
 	mv x2, a0
 	ret
 
-timer_clear:
-	#ld t0, 0xFFFFFFFFFFFFFFFF
-	csrc mcycle, t0
+cycle_clear:
+	ld t0, FF64
+	csrrc x0, mcycle, t0
 	ret
 
-timer:
-	csrr a0, mcycle
+cycle:
+	csrrs a0, mcycle, x0
 	ret
 
 write_result:
--- Memory Perfomance Test/main.cpp
+++ Memory Perfomance Test/main.cpp
@@ -6,23 +6,34 @@
 
 */
 
-//#include <cmath>
+#define __RESULT    __attribute__((section (".result"))) __attribute__((aligned(8))) = {0}
+# define N 10000
 
 void test1();
 
-unsigned int N = 10000;
-long unsigned int s_pointer = 0x10020000;
+//long unsigned int N = 10000;
+long unsigned int s_pointer = 0x10400000;
+//unsigned int result = 0;
+//unsigned int res __RESULT;
+int res ;//__RESULT;
+//unsigned int res_mas[10] __RESULT;
 
 extern "C"
 {
 	void setup_stack_pointer(long unsigned int);
-	unsigned long int timer(void);
+	unsigned long int cycle(void);
 	void write_result(int number);
+	void cycle_clear(void);
 }
 
 int main()
 {
-	setup_stack_pointer(s_pointer);
+	//setup_stack_pointer(s_pointer);
+	//asm(" ld x2, s_pointer ");
+
+	//Включение FPU
+	asm("li x10, 0x2000");
+	asm("CSRRS x0, mstatus, x10");//0x300
 
 	test1();
 
@@ -33,28 +44,26 @@ int main()
 
 void test1()
 {
-	unsigned long int time1;
-	unsigned long int time2;
+	unsigned long int time1 = 0;
+	unsigned long int time2 = 0;
+	int i = 0;
 
-	// write zero in timer
-	//timer_clear();
-
-	time1 = timer(); // run timer first time
+	cycle_clear();
 
 	long int memory[N];
 
-	for (int i=0; i < N; i++)
+	time1 = cycle();
+
+	for (i=1; i <= N; i++)
 	{
-		memory[i] = i+1;
+		memory[i] = i;
 	}
 
-	//timer_clear();
-	time2 = timer();
+	time2 = cycle();
+	//N = 10000;
+	res = (time2 - time1) / N;
+	//res = result;
 
-	//result = std::abs((time2 - time1)) / N;
-	double result = (double) (time2 - time1) / N;
-	long int final = (long int) result;
-	write_result(final);
 
 	for(;;);
 }
